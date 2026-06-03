5228e789194049c570778b788207513915731d3e
Saai Namballa <saainamballa2468@gmail.com>
2026-06-03 16:05:49 +0530

    Add files via upload

---
+++ Learning the basics/learn_the_basics_01.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//iostream is a liibrary contains input nd output functions
+#include<bits\stdc++.h>
+//This libray will include everything
+using namespace std;
+int main(){
+    int c,d;
+    cout << "Yoo enter the value of c and d : ";
+    cin >> c >> d ;
+    cout << "Heyy there this is Saai Namballa" << "\n" << c << " and " << d << " are the value of c and d";
+    return 0;
+}
---
+++ Learning the basics/learn_the_basics_02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int, double, float, long, long long - for storing numbers
+    //string, getline
+    //char
+    string str;
+    getline(cin,str);
+    cout << str ;
+    //ranges of space for data types are
+    // int - 10^-8 to 10^8
+    // long - 10^-12 to 10^12
+    // long long - 10^-18 to 10^18 
+    return 0;
+}
